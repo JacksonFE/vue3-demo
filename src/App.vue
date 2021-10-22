86f40a395a890f89f605b69b9713e84d1a1ae7d1
WuZhanPeng <jackson.wzp@outlook.com>
2021-10-22 11:04:29 +0800

    composition api - expose

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <HelloWorld />
+  <div>
+    <button @click="handleClick">child increment</button>
+    <HelloWorld ref="child"/>
+  </div>
 </template>
 
 <script>
@@ -9,6 +12,12 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    handleClick() {
+      // 可以访问子组件的increment方法，但是无法直接访问count，因为count未被导出
+      this.$refs.child.increment()
+    }
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,27 @@
 <template>
-  <div></div>
+  <div>{{count}}</div>
 </template>
 
 <script>
 import {ref} from 'vue'
 export default {
   name: "HelloWorld",
+  // expose: ['increment'],
+  setup(props, {expose}) {
+    const count = ref(0)
+    const increment = () => {
+      count.value ++
+    }
+    // 向外暴露的属性，外部通过$refs/$parent/$root调用当前组件时只有被暴露的属性才能被访问到
+    // 可以保持组件的封装性，避免外部随意访问/修改当前组件的属性
+    expose({
+      increment
+    })
+    return {
+      count,
+      increment
+    }
+  }
 };
 </script>
 
