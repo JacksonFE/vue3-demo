d7a0edefade1bca7eca748bb9eb9c6eca7522853
Jackson Wu <jackson.wzp@outlook.com>
2021-10-28 00:17:24 +0800

    provide/inject, refs

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,51 @@
 <template>
   <div>
     <button @click="handleClick">child increment</button>
-    <HelloWorld ref="child"/>
+    <HelloWorld ref="child" />
+    <button @click="testRef">test ref</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { provide, ref, onMounted } from "vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  setup() {
+    const name = ref("name");
+    provide("name", name);
+    // setTimeout(() => {
+    //   name.value = 'new name'
+    // }, 2000)
+    const child = ref(null);
+    const handleClick = () => {
+      child.value.increment();
+    };
+    onMounted(() => {
+      console.log(child.value);
+    });
+    return {
+      name,
+      handleClick,
+      // 虚拟DOM补丁算法中VNode的ref属性值如果和render context中的ref对应，那么在虚拟DOM挂载/patch过程中会将该虚拟DOM赋给render context中对应的ref
+      // 此处通过setup return将child暴露到render context中，因此挂载后template中的ref='child'节点会被赋给下面的child
+      child,
+    };
   },
   methods: {
-    handleClick() {
-      // 可以访问子组件的increment方法，但是无法直接访问count，因为count未被导出
-      this.$refs.child.increment()
-    }
-  }
-}
+    // handleClick() {
+    // 可以访问子组件的increment方法，但是无法直接访问count，因为count未被导出
+    // this.$refs.child.increment()
+    // },
+    testRef() {
+      // setup中return出来的ref可以通过以下方式修改
+      this.name = "1";
+      console.log(this.name);
+    },
+  },
+};
 </script>
